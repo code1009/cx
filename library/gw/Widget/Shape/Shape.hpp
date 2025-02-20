037cb4c0da91f137e2f3e962a0b85b8e181d62ab
code1009 <code1009@naver.com>
2025-02-20 09:40:22 +0900

    1

---
+++ library/gw/Widget/Shape/Shape.hpp
@@ -0,0 +1,71 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#pragma once
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+namespace cx::gw
+{
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+class Shape: public Widget
+{
+protected:
+	WidgetDocument* _WidgetDocument{ nullptr };
+
+protected:
+	WidgetFillStyle _FillStyle{ Color{0.7f, 1.0f, 0.7f, 1.0f} };
+	WidgetLineStyle _LineStyle{ Color{0.0f, 0.0f, 0.0f, 1.0f}, 3.0f, 0, 0 };
+	WidgetTextStyle _TextStyle;
+
+protected:
+	WidgetResource* _Fill_Brush     { nullptr };
+	WidgetResource* _Line_Brush     { nullptr };
+	WidgetResource* _Text_TextFormat{ nullptr };
+	WidgetResource* _Text_Brush     { nullptr };
+
+protected:
+	std::wstring _Text{ L"아름다운 한글" };
+
+public:
+	explicit Shape(std::wstring className);
+
+public:
+	virtual ~Shape() = default;
+
+	//-----------------------------------------------------------------------
+	// Widget
+public:
+	virtual void copyTo(WidgetSharedPtr widget) const override;
+	virtual bool loadResources(WidgetResourceMap* widgetResourceMap) override;
+
+	//-----------------------------------------------------------------------
+public:
+	virtual std::wstring getText(void) const;
+	virtual void setText(std::wstring text);
+	virtual void setWidgetDocument(WidgetDocument* doc);
+
+public:
+	virtual void onWidgetDrawingStyleChanged(WidgetDrawingStyle::StyleChangedParam* param);
+};
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+}
+
+
+
+
