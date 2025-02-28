76d316e30bdcbfdf2e5469a487be8527133c6bfc
code1009 <code1009@naver.com>
2025-02-28 19:28:14 +0900

    1

---
+++ template/WebUIApplication/src/Main.cpp
@@ -0,0 +1,47 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#include "pch.hpp"
+
+//===========================================================================
+#include <wui/wui.hpp>
+#include <gw/gw.hpp>
+#include <runtime/runtime.hpp>
+
+//===========================================================================
+#include "Application.hpp"
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+int APIENTRY wWinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPWSTR    lpCmdLine,
+	int       nCmdShow)
+{
+	CX_RUNTIME_LOG(LInfo)
+		<< L"START" << std::endl;
+
+
+	cx::wui::getAppModule()->setInstanceHandle(hInstance);
+
+	
+	getApplication()->launch();
+
+
+	CX_RUNTIME_LOG(LInfo)
+		<< L"END" << std::endl;
+	
+	return 0;
+}
---
+++ template/WebUIApplication/src/Window/View.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+class View : 
+	public cx::wui::MessageMapWindowT<View, cx::wui::BaseWindow>
+{
+private:
+	std::unique_ptr<cx::gw::BaseEditWindow> _Window;
+
+public:
+	explicit View(HWND parentWindowHandle);
+
+public:
+	virtual HWND createView(HWND parentWindowHandle);
+
+public:
+	void registerWindowMessageMap(void);
+	void onCreate(cx::wui::WindowMessage& windowMessage);
+	void onDestroy(cx::wui::WindowMessage& windowMessage);
+	void onClose(cx::wui::WindowMessage& windowMessage);
+	void onSize(cx::wui::WindowMessage& windowMessage);
+	void onHScroll(cx::wui::WindowMessage& windowMessage);
+	void onVScroll(cx::wui::WindowMessage& windowMessage);	
+	void onMouseWheel(cx::wui::WindowMessage& windowMessage);
+	void onMouseMove(cx::wui::WindowMessage& windowMessage);
+	void onKeyDown(cx::wui::WindowMessage& windowMessage);
+	void onEraseBkgnd(cx::wui::WindowMessage& windowMessage);
+	void onPaint(cx::wui::WindowMessage& windowMessage);	
+	void onCommand(cx::wui::WindowMessage& windowMessage);
+	void onMenuCommand(cx::wui::WindowMessage& windowMessage);
+	void onCtlCommand(cx::wui::WindowMessage& windowMessage);
+
+public:
+	void onIdle(void);
+};
