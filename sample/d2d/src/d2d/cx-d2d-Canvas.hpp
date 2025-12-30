7572aad35c3d2067cdefd5f04d14e40048b70edd
code1009 <code1009@naver.com>
2025-12-31 00:24:24 +0900

    1

--- sample/d2d/src/d2d/cx-d2d-Canvas.hpp
+++ sample/d2d/src/d2d/cx-d2d-Canvas.hpp
@@ -23,9 +23,9 @@ public:
 	std::function<void(DrawingSession*)> onDraw;
 
 private:
-	std::unique_ptr<cx::d2d::Context> _Context;
-	std::unique_ptr<cx::d2d::Renderer> _Renderer;
-	std::unique_ptr<cx::d2d::DrawingSession> _DrawingSession;
+	std::unique_ptr<Context> _Context;
+	std::unique_ptr<Renderer> _Renderer;
+	std::unique_ptr<DrawingSession> _DrawingSession;
 
 public:
 	explicit Canvas(Factory* factory, HWND hwnd, std::uint32_t cx, std::uint32_t cy);
