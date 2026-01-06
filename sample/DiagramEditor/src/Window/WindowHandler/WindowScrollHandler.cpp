5d5b860c153382073b0699afca1e5d0d89c2c774
code1009 <code1009@naver.com>
2026-01-06 19:21:16 +0900

    1

---
+++ sample/DiagramEditor/src/Window/WindowHandler/WindowScrollHandler.cpp
@@ -0,0 +1,41 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+#include "pch.hpp"
+
+//===========================================================================
+#include <cx/wui/wui.hpp>
+#include <cx/runtime/runtime.hpp>
+
+//===========================================================================
+#include "WindowScrollHandler.hpp"
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+WindowScrollHandler::WindowScrollHandler(HWND hwnd) :
+	_Hwnd(hwnd)
+{
+}
+
+//===========================================================================
+bool WindowScrollHandler::onWindowMessage(cx::wui::WindowMessage& windowMessage)
+{
+	bool handled = false;
+	onWindowScrollMessage(windowMessage, handled);
+	return handled;
+}
+
+void WindowScrollHandler::onWindowScrollMessage(cx::wui::WindowMessage& windowMessage, bool& handled)
+{
+	switch (windowMessage.uMsg)
+	{
+	//-----------------------------------------------------------------------
+	case WM_MOUSEWHEEL:
+
+	default:
+		break;
+	}
+}
---
+++ sample/DiagramEditor/src/Window/WindowHandler/WindowScrollHandler.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//===========================================================================
+class WindowScrollHandler
+{
+private:
+	HWND _Hwnd{ nullptr };
+
+public:
+	explicit WindowScrollHandler(HWND hwnd);
+
+public:
+	WindowScrollHandler(const WindowScrollHandler& other) = delete;
+	WindowScrollHandler& operator=(const WindowScrollHandler& other) = delete;
+	WindowScrollHandler(WindowScrollHandler&& other) noexcept = delete;
+	WindowScrollHandler& operator=(WindowScrollHandler&& other) noexcept = delete;
+
+public:
+	virtual bool onWindowMessage(cx::wui::WindowMessage& windowMessage);
+	virtual void onWindowScrollMessage(cx::wui::WindowMessage& windowMessage, bool& handled);
+};
+
